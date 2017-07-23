dbfb04ea1638aee2fa669ad2262ab498b8027c17
Marcos E. Torres <metorres89@gmail.com>
2017-07-23 17:34:19 -0300

    Se separa la lógica del ataque de EnemyState. Se agrega EnemyAttack. Se agrega delay entre los ataques del enemigo para que no mate al jugador tan rápido.

---
+++ Assets/scripts/gameplay/EnemyAttack.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+	[SerializeField] private float minDamage = 10.0f;
+	[SerializeField] private float maxDamage = 30.0f;
+	[SerializeField] private float playerDistanceAttackTrigger = 2.0f;
+	[SerializeField] private float attackTimer = 1.0f;
+
+	private GameObject myPlayerRef;
+	private EnemyState myEnemyState;
+
+	public float attackDelay = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		myPlayerRef = GameObject.FindGameObjectWithTag ("Player");
+		myEnemyState = gameObject.GetComponent<EnemyState> ();
+		attackTimer = attackDelay;
+	}
+	
+	public void FixedUpdate () {
+		
+		if (myEnemyState.isAlive) {
+			float distanceFromPlayer = Vector3.Distance (gameObject.transform.position, myPlayerRef.transform.position);
+
+			if (distanceFromPlayer <= playerDistanceAttackTrigger) {
+
+				attackTimer -= Time.fixedDeltaTime;
+
+				if (attackTimer <= 0.0f) {
+					Attack ();
+					attackTimer = attackDelay;
+				}
+
+			} 	
+		}
+	}
+
+	private void Attack() {
+
+		float currentAttackDamage = Random.Range (minDamage, maxDamage);
+
+		PlayerState playerState = myPlayerRef.GetComponent<PlayerState> ();
+
+		Debug.LogFormat ("Enemy is attacking player , player will take damage: {0}", currentAttackDamage);
+
+		playerState.ReceiveDamage (currentAttackDamage);
+	}
+}
