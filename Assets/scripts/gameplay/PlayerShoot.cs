8a13887c9663af4c255ed535e7a5c5e54d7a1db5
Marcos E. Torres <metorres89@gmail.com>
2017-07-24 21:56:59 -0300

    Se agregó un limite al disparo del player para que no esté disparando en cada fixed update.

--- Assets/scripts/gameplay/PlayerShoot.cs
+++ Assets/scripts/gameplay/PlayerShoot.cs
@@ -3,28 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof (PlayerState))]
+[RequireComponent(typeof (ParticleSystem))]
+
 public class PlayerShoot : MonoBehaviour {
 
 	[SerializeField] private float shootMaxDistance = 500.0f;
 	[SerializeField] private float shootDamage = 1.0f;
+	[SerializeField] private float shootRate = 0.25f;
 
+	private float myShootTimer;
 	private PlayerState myPlayerState;
 
 	public void Start() {
 		myPlayerState = gameObject.GetComponent<PlayerState> ();
+		myShootTimer = shootRate;
 	}
 
 	public void FixedUpdate () {
 		if (Input.GetAxis ("Fire1") != 0.0f && myPlayerState.isAlive) {
-			RaycastHit hitInfo;
-			bool hasHit = Physics.Raycast (Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, shootMaxDistance);
 
-			if (hasHit) {
-				if (hitInfo.collider.gameObject.tag == "Enemy") {
-					EnemyState enemyState = hitInfo.collider.gameObject.GetComponent<EnemyState> ();
+			myShootTimer -= Time.fixedDeltaTime;
 
-					enemyState.ReceiveDamage (shootDamage);
+			if (myShootTimer <= 0.0) {
+				
+				RaycastHit hitInfo;
+				bool hasHit = Physics.Raycast (Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, shootMaxDistance);
+
+				if (hasHit) {
+					if (hitInfo.collider.gameObject.tag == "Enemy") {
+						EnemyState enemyState = hitInfo.collider.gameObject.GetComponent<EnemyState> ();
+						enemyState.ReceiveDamage (shootDamage);
+					}
 				}
+
+				myShootTimer = shootRate;
 			}
 		}
 	}
