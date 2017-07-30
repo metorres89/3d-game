9bd21c70f96578e9a0b8a70348d1f50c2cdac186
Marcos E. Torres <metorres89@gmail.com>
2017-07-30 19:36:12 -0300

    Se agregó la capacidad de dropear municiones o helathpacks a los enemigos.

---
+++ Assets/scripts/gameplay/enemy/DroppableItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DroppableItem : System.Object
+{
+	public GameObject itemPrototype;
+	[Range(0.0f, 1.0f)] public float chanceToDrop;
+}
---
+++ Assets/scripts/gameplay/enemy/EnemyItemDrop.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (EnemyState))]
+public class EnemyItemDrop : MonoBehaviour {
+
+	[SerializeField] [Range(0.0f, 1.0f)]private float chanceToDropItem = 0.25f;
+	public DroppableItem[] droppableItems;
+
+	private float totalChance;
+	private bool alreadyDropped;
+	private EnemyState myEnemyState;
+
+	private void DropRandomItem() {
+		float dropChance = Random.Range (0.0f, 1.0f);
+
+		if (dropChance <= chanceToDropItem) {
+			float itemChance = Random.Range (0.0f, totalChance);
+
+			for (int index = 0; index < droppableItems.Length; index++) {
+				if (itemChance <= droppableItems [index].chanceToDrop) {
+					//drop item from this index;
+					BuildNewItemDrop(index);
+					break;
+				} else {
+					itemChance -= droppableItems [index].chanceToDrop;
+				}
+			}
+		}
+	}
+
+	private float GetTotalChances() {
+		float totalChances = 0.0f;
+		foreach (DroppableItem element in droppableItems) {
+			totalChances += element.chanceToDrop;
+		}
+		return totalChances;
+	}
+
+	private void BuildNewItemDrop(int itemIndex){
+		GameObject newItem = Instantiate (droppableItems [itemIndex].itemPrototype, gameObject.transform.position, gameObject.transform.rotation);
+	}
+
+	public void Start(){
+		myEnemyState = gameObject.GetComponent<EnemyState> ();
+		totalChance = GetTotalChances ();
+		alreadyDropped = false;
+	}
+
+	public void Update() {
+		if (myEnemyState.isAlive == false && alreadyDropped == false && chanceToDropItem > 0.0f && droppableItems.Length > 0) {
+			DropRandomItem ();
+			alreadyDropped = true;
+		}
+	}
+}
