8eee3d002631d807017bc4a52ae40aa827e6c595
Marcos E. Torres <metorres89@gmail.com>
2017-08-02 02:55:06 -0300

    Se agregó menú principal que lleva a la escena de gameplay. Se agregó escena de game_result pero todavía no se conectó al gameplay.

---
+++ Assets/scripts/main_menu/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public Button startButton;
+	public Button settingsButton;
+
+	public void Start () {
+		startButton.onClick.AddListener (StartGame);
+		settingsButton.onClick.AddListener (Settings);
+	}
+
+	public void StartGame() {
+		SceneManager.LoadScene ("gameplay", LoadSceneMode.Single);
+	}
+
+	public void Settings() {
+		Debug.Log ("Settings not implemented yet!");
+	}
+}
